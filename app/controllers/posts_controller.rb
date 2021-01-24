f503b2e03ed98fede9f61473cfcb9c0e3031e1ae
HiroMiyagi <isniilevsata_2012@yahoo.co.jp>
2021-01-24 16:07:23 +0900

    既読機能のフロントエンド実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -9,7 +9,7 @@ class PostsController < ApplicationController
   end
 
   def checked
-    post = Post.find(param[:id])
+    post = Post.find(params[:id])
     if post.checked
       post.update(checked: false)
     else
